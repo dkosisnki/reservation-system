b8423945073a7aeeb9a9e8930a3f0c75c7d7727c
Damian <d.kosinski13@wp.pl>
2023-07-06 09:23:39 +0200

    Initial commit

---
+++ src/test/java/com/luxmed/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxmed.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
